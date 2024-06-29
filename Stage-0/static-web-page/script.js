1dfae47be52cc509fd806a5a23b1cfd73e195738
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2024-06-29 17:48:13 +0100

    Changed Username

--- Stage-0/static-web-page/script.js
+++ Stage-0/static-web-page/script.js
@@ -1,4 +1,4 @@
 document.getElementById("name").textContent = "Great-Victor Anjorin";
-document.getElementById("username").textContent = "Victor the Great";
+document.getElementById("username").textContent = "VictortheGreat";
 document.getElementById("email").textContent = "greatvictor.anjorin@gmail.com";
 document.getElementById("email").href = "mailto:greatvictor.anjorin@gmail.com";
